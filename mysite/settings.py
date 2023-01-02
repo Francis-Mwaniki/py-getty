8de9241ffa42fbd2a5a1998094e7cc12fd9e9e27
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-02 11:58:47 +0300

    using dbsqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,6 +131,7 @@ DATABASES = {
 }
  
 
+
 #EMAIL SETTING
 #
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -180,7 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-#STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
