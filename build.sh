601c196ef50bbd4842c994be1dc6deda900070e9
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-02 11:45:54 +0300

    using dbsqlite

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+set -o errexit  # exit on error
 
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+pip install -r requirements.txt
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --no-input
+python manage.py migrate
--- mysite/settings.py
+++ mysite/settings.py
@@ -118,11 +118,14 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
  
 
